d6b3c5b94eef3de95c16a074dc461b7df87071b8
Namoj-design <namojperiakumar@gmail.com>
2026-01-26 14:42:35 +0530

    new version

--- core/pad/pad_generator.py
+++ core/pad/pad_generator.py
@@ -1,18 +1,8 @@
 # core/pad/pad_generator.py
 
-import uuid
 from core.entropy.utils import bits_to_bytes
-from core.pad.pad_hash import hash_pad
-from core.pad.pad_store import store_pad
-from core.pad.pad_registry import register_pad
 
 
-def create_pad_from_bits(bits, owner="local-user"):
+def create_pad_from_bits(bits) -> bytes:
     pad_bytes = bits_to_bytes(bits)
-    pad_id = str(uuid.uuid4())
-
-    pad_hash = hash_pad(pad_bytes)
-    store_pad(pad_bytes, pad_id)
-    register_pad(pad_id, pad_hash, owner)
-
-    return pad_id, pad_hash
+    return pad_bytes
